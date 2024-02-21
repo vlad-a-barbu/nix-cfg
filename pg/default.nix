19e96e8bd4701f90400ef896f7751589e6af2a61
vb <vlad.a.brb@gmail.com>
2024-02-21 21:08:13 +0200

    other useful refs

--- pg/default.nix
+++ pg/default.nix
@@ -1,1 +1,1 @@
-({a, b, c ? (x: x + 3), ... }@args: c (a + b + args.e)) { a = 1; b = 2; d = 4; e = 7; }
+({a, b, c ? (x: x + 3), ... }@args: c (a + b + args.e)) { a = 1; b = 2; c = (x: x * 3); d = 4; e = 7; }
